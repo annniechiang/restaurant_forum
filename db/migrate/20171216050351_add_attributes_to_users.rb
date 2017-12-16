883babf32d011c3f8b7a2f44633ba912f8f16ad4
annie chiang <annie100patty@gmail.com>
2017-12-16 13:09:15 +0800

    add attributes to user model

---
+++ db/migrate/20171216050351_add_attributes_to_users.rb
@@ -0,0 +1,6 @@
+class AddAttributesToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :intro, :text
+    add_column :users, :avatar, :string # 用來掛載 CarrierWave 的 Uploader
+  end
+end
