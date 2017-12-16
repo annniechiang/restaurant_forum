18718a5e44ad68bdfaeea9b0faa8615a09daf6f1
annie chiang <annie100patty@gmail.com>
2017-12-16 18:31:33 +0800

    add edit page to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,34 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
+  end
+
+  def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your profile was successfully updated"
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "Your profile was failed to update"
+      render :edit 
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
   end
 
 end
