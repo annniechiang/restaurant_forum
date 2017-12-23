95acc586ea6b416ab203e0baff1699bbfca5c0f2
annie chiang <annie100patty@gmail.com>
2017-12-24 00:36:33 +0800

    create like model / setup relation among user, restaurant and like

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,15 @@ class User < ApplicationRecord
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
 
-  # 「餐廳被很多使用者收藏」的多對多關聯
-  # 將關聯名稱自訂為 :favorited_users
+  # 「使用者收藏很多餐廳」的多對多關聯
+  # 由於使用 restaurants 會和「使用者評論很多餐廳」重覆，將關聯名稱自訂為 :favorited_restaurants
   # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
+
+  # 「使用者喜歡很多餐廳」的多對多關聯
+  has_many :likes, dependent: :destroy
+  has_many :liked_restaurants, through: :likes, source: :restaurant
 
   def admin?
     self.role == "admin"
