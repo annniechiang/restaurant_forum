8fb20e7889b8150448f03ee907a9337205715da8
annie chiang <annie100patty@gmail.com>
2017-12-26 15:14:31 +0800

    add count_favorites method and ranking page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,9 +19,14 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def ranking
+    @restaurants = Restaurant.order(favorites_count: :desc).limit(10)
+  end
+
   def favorite
     @restaurant = Restaurant.find(params[:id])
     @restaurant.favorites.create!(user: current_user)
+    @restaurant.count_favorites
     redirect_back(fallback_location: root_path)  # 導回上一頁
   end
 
@@ -29,6 +34,7 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     favorite = Favorite.where(restaurant: @restaurant, user: current_user)
     favorite.destroy_all
+    @restaurant.count_favorites
     redirect_back(fallback_location: root_path)
   end
 
